de75582138b13885f77319cbe37d6a8d8800f798
Jakub Ogrodnik <81594386+JakubOgrodnikPL@users.noreply.github.com>
2023-03-29 10:39:13 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -8,4 +8,16 @@ void strandSort(list<int> &ip, list<int> &op)
     list<int> sublist;
     sublist.push_back(ip.front());
     ip.pop_front();
+
+    for (auto it = ip.begin(); it != ip.end(); ) {
+        if (*it > sublist.back()) {
+            sublist.push_back(*it);
+            it = ip.erase(it);
+        }
+        else
+        it++;
+    }
+    
+    op.merge(sublist);
+    strandSort(ip, op);
 }
